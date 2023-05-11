0474f49c61d81158135c3bc95e11fa05e8a7f5ba
smlsvnssn <samuel@lhli.net>
2023-05-11 16:24:26 +0200

    removed junk

--- src/lib/@threlte_hacked/rapier/dist/lib/rotationToEuler.d.ts
+++
@@ -1,6 +0,0 @@
-import type { Rotation } from '$lib/@threlte_hacked/core'
-import { Euler } from 'three'
-export declare const rotationToEuler: (
-	rotation?: Rotation,
-	euler?: Euler,
-) => Euler
--- src/lib/@threlte_hacked/rapier/dist/lib/rotationToQuaternion.d.ts
+++
@@ -1,6 +0,0 @@
-import type { Rotation } from '$lib/@threlte_hacked/core'
-import { Quaternion } from 'three'
-export declare const rotationToQuaternion: (
-	rotation?: Rotation,
-	quaternion?: Quaternion,
-) => Quaternion
