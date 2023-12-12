e0f3fa87d2e40c33467d2f1ec3f5ee616223de2a
smlsvnssn <samuel@lhli.net>
2023-12-12 21:43:02 +0100

    fixeli scroll

--- src/routes/lfWrapped/+layout.svelte
+++ src/routes/lfWrapped/+layout.svelte
@@ -1,34 +1,18 @@
 <script>
 	import '../../style.css'
-	import logo from '$lib/images/logotyp-page-1.svg'
 </script>
 
 <svelte:head>
 	<title>LF Wrapped 2025</title>
 </svelte:head>
-<nav>
-	<a href="/" class="logo">
-		<img class="mb" src={logo} alt="" />LF Wrapped 2025
-	</a>
-	<ul>
-		<li></li>
-		<li class="active"></li>
-		<li></li>
-		<li></li>
-		<li></li>
-		<li></li>
-		<li></li>
-		<li></li>
-	</ul>
-</nav>
+
 <main class="lb4">
-	<div class="fejkbody">
-		<slot />
-	</div>
+	<slot />
 </main>
 
 <style lang="scss">
 	:global(body) {
+		--headerHeight: 0;
 		background: linear-gradient(0deg, #001d3a 0%, #001d3a 100%) var(--tint) !important;
 		height: calc(100svh - var(--headerHeight)) !important;
 		inset: 0;
@@ -40,43 +24,7 @@
 		-moz-osx-font-smoothing: grayscale;
 		text-rendering: optimizeLegibility;
 	}
-	a {
-		text-decoration: none;
-	}
-	nav {
-		position: absolute;
-		margin: 2rem;
-		width: calc(100% - 4rem);
-		.logo {
-			display: flex;
-			align-items: center;
-			gap: 0.5rem;
-			color: #fff;
 
-			font-family: LF Rubrik;
-			font-size: 1.25rem;
-			font-style: normal;
-			font-weight: 400;
-			line-height: 0.9375rem; /* 75% */
-		}
-		ul {
-			display: flex;
-			margin-top: 2rem;
-			padding: 0;
-			gap: 2px;
-			justify-items: stretch;
-			flex-direction: row;
-			li {
-				flex: 1;
-				background: #fff8;
-				height: 0.125rem;
-
-				&.active {
-					background: #fff;
-				}
-			}
-		}
-	}
 	:global(.btn) {
 		position: initial !important;
 	}
@@ -85,16 +33,13 @@
 		color: var(--white);
 	}
 	main {
-		height: 100dvh;
+		height: 100vh;
+		width: 100vw;
+
+		background: linear-gradient(0deg, #001d3a 0%, #001d3a 100%) var(--tint) !important;
+
 		display: grid;
-		align-items: center;
-		justify-items: center;
-	}
-	.fejkbody {
-		max-width: 480px;
-		margin: auto;
-		display: flex;
-		min-height: 700px;
-		align-content: stretch;
+		align-content: center;
+		justify-content: center;
 	}
 </style>
--- src/routes/lfWrapped/+page.svelte
+++ src/routes/lfWrapped/+page.svelte
@@ -1,16 +1,41 @@
 <script>
 	import mat from '$lib/images/mat-mat.svg'
 	import up from '$lib/images/arrow-growth-20.svg'
+	import save from '$lib/images/produktillustrationer-mobil-spara-370-x-200.svg'
+	import earth from '$lib/images/klimat-och-vder-hllbar-vrld.svg'
+	import jul from '$lib/images/GOJUL.svg'
+	import logo from '$lib/images/logotyp-page-1.svg'
+	import { onMount } from 'svelte'
 
 	import { isSmallWindow } from '$lib/globals'
 
 	export const prerender = true
-	let innerWidth
 
-	$: $isSmallWindow = innerWidth < 800
+	let scroller
+
+	let activeIndex = 0,
+		numItems
+
+	const checkScroll = () => {
+		activeIndex = Math.round(scroller.scrollLeft / scroller.clientWidth)
+		numItems = scroller.scrollWidth / scroller.clientWidth
+	}
+
+	onMount(checkScroll)
 </script>
 
-<div class="wrapper">
+<nav>
+	<a href="/" class="logo">
+		<img class="mb" src={logo} alt="" />LF Wrapped 2025
+	</a>
+	<ul>
+		{#each [...Array(numItems).keys()] as index}
+			<li class:active={activeIndex === index}></li>
+		{/each}
+	</ul>
+</nav>
+
+<div class="wrapper" on:scroll={checkScroll} bind:this={scroller}>
 	<article>
 		<img class="mb" src={mat} alt="" />
 		<h5>Du har gjort</h5>
@@ -27,16 +52,135 @@
 			>Hitta bra restauranger nära dig</button
 		>
 	</article>
+	<article>
+		<img class="mb" src={save} alt="" />
+		<h5>Du har sparat</h5>
+		<h1>34 319</h1>
+		<h5>kronor i år</h5>
+		<hr />
+		<p>
+			<img src={up} alt="" />
+			<span>1.4%</span> mer än förra året
+		</p>
+		<button type="button" class="btn btn-secondary"
+			>Starta ett fondkonto för sparandet</button
+		>
+	</article>
+	<article>
+		<img class="mb" src={earth} alt="" />
+		<h5>Ditt sparande är</h5>
+		<h1>17%</h1>
+		<h5>hållbart</h5>
+		<hr />
+		<p>
+			<img src={up} alt="" />
+			<span>1.4%</span> mer än förra året
+		</p>
+		<hr />
+		<p class="b">Här finns förbättringspotential!</p>
+		<p>
+			Hållbara fonder är inte bara bra för planeten, utan också för
+			plånboken, enligt <a href="">MorningStar</a>,
+			<a href="">Dagens Industri</a>, <a href="">Financial Times</a> och
+			<a href="">FN</a>.
+		</p>
+		<button type="button" class="btn btn-secondary"
+			>Hitta hållbara fonder</button
+		>
+	</article>
+	<!-- <article>
+		<img class="mb" src={mat} alt="" />
+		<h5>genomfört</h5>
+		<h1>367</h1>
+		<h5>onödiga transaktioner</h5>
+		<hr />
+		<p>
+			<img src={up} alt="" />
+			<span>6.4%</span> fler än förra året
+		</p>
+		<hr />
+		<p class="b">Här finns förbättringspotential!</p>
+		<p>
+			Hållbara fonder är inte bara bra för planeten, utan också för
+			plånboken, enligt <a href="">MorningStar</a>,
+			<a href="">Dagens Industri</a>, <a href="">Financial Times</a> och
+			<a href="">FN</a>.
+		</p>
+		<button type="button" class="btn btn-secondary"
+			>Hitta hållbara fonder</button
+		>
+	</article> -->
+	<article>
+		<img class="mb" src={jul} alt="" />
+		<h5>Du har fått</h5>
+		<h1>1 897</h1>
+		<h5>kronor i återbäring</h5>
+		<hr />
+		<p>
+			<img src={up} alt="" />
+			<span>3.7%</span> mer än förra året
+		</p>
+		<hr />
+		<h5>God jul och gott nytt år från oss på LF Halland. Vi ses!</h5>
+		<button type="button" class="btn btn-secondary"
+			>Sätt in återbäringen på sparkontot</button
+		>
+	</article>
 </div>
 
 <style lang="scss">
 	* {
 		hyphens: auto;
 	}
+
+	nav {
+		position: absolute;
+		margin: 2rem;
+		width: calc(100% - 4rem);
+
+		.logo {
+			display: flex;
+			align-items: center;
+			gap: 0.5rem;
+			color: #fff;
+			background: none;
+			text-decoration: none;
+
+			font-family: LF Rubrik;
+			font-size: 1.25rem;
+			font-style: normal;
+			font-weight: 400;
+			line-height: 0.9375rem; /* 75% */
+		}
+		ul {
+			display: flex;
+			margin-top: 2rem;
+			padding: 0;
+			gap: 2px;
+			justify-items: stretch;
+			flex-direction: row;
+			li {
+				flex: 1;
+				background: #fff8;
+				height: 0.125rem;
+				list-style: none;
+				transition: background 0.5s;
+
+				&.active {
+					background: #fff;
+				}
+			}
+		}
+	}
+
 	.wrapper {
 		display: flex;
-		flex-direction: column;
-		width: 100%;
+		flex-direction: row;
+		align-items: center;
+		//max-width: 480px;
+		min-height: 700px;
+		scroll-snap-type: x mandatory;
+		overflow-y: scroll;
 
 		img {
 			max-width: 20rem;
@@ -44,7 +188,9 @@
 		}
 
 		article {
+			flex: 1 0 100%;
 			text-align: center;
+			scroll-snap-align: center;
 		}
 
 		h1,
@@ -64,10 +210,14 @@
 		}
 
 		p {
+			max-width: 80%;
 			font-family: Intro Cond;
-			margin: 0;
+			margin: auto;
 		}
 
+		a {
+			color: var(--cloud);
+		}
 		hr {
 			border: 0;
 			margin: 1.5rem;
